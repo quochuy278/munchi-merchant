e1f7ddd8b81397dcb06b9d51179789e3148fdf74
quochuy278 <quochuy270800@gmail.com>
2023-02-02 15:12:40 +0200

    add useForm for validation

--- src/utils/validateInput.ts
+++ src/utils/validateInput.ts
@@ -1,5 +1,6 @@
 import { ValidateResult } from "../shared/interfaces/services.interface";
-
+import { literal, object, string, TypeOf } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 export function validateSignInData(
   email: unknown,
   password: unknown
@@ -40,8 +41,26 @@ export function validateSignInData(
       };
   }
 }
+export const signInSchema = object({
+  email: string().email('Email is invalid'),
+  password: string()
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+});
 
-// export function validateSignUpData(email:unknown, password:unknown):ValidateResult{
 
-//     return true
-// }
+export const signUpSchema = object({
+  name: string()
+    .max(32, 'Name must be less than 100 characters'),
+  email: string().email('Email is invalid'),
+  password: string()
+    .min(8, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+  passwordConfirm: string().nonempty('Please confirm your password'),
+  terms: literal(true, {
+    invalid_type_error: 'Accept Terms is required',
+  }),
+}).refine((data) => data.password === data.passwordConfirm, {
+  path: ['passwordConfirm'],
+  message: 'Passwords do not match',
+});
